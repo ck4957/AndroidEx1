8e29312eaec90aa98f77e09aa93701084f1d8112
Chirag Kular <ck4957@rit.edu>
2018-06-08 16:18:20 -0400

    Device Secure Changes

--- app/src/main/java/com/example/chiragkular/androidex1/MainActivity.java
+++ app/src/main/java/com/example/chiragkular/androidex1/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.chiragkular.androidex1;
 
 import android.Manifest;
+import android.app.KeyguardManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -16,9 +20,11 @@ public class MainActivity extends AppCompatActivity {
 
     public static final int REQUEST_CALENDAR = 0;
     public static final String EXTRA_MESSAGE = "com.example.chiragkular.MESSAGE";
+    private static Context mContext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        readOSVersion(getApplicationContext());
         setContentView(R.layout.activity_main);
     }
 
@@ -27,11 +33,29 @@ public class MainActivity extends AppCompatActivity {
         EditText editText = (EditText) findViewById(R.id.editText);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
+
         startActivity(intent);
     }
 
     public void readCalendar(){
 
+    }
+
+    public void readOSVersion(Context context){
+        KeyguardManager km = (KeyguardManager) context.getSystemService(Context.KEYGUARD_SERVICE);
+        boolean deviceSecure = km.isDeviceSecure();
+        String manufacturer = Build.MANUFACTURER;
+        String model = Build.MODEL;
+        int version = Build.VERSION.SDK_INT;
+        String versionRelease = Build.VERSION.RELEASE;
+
+
+        Log.e("MyActivity", "manufacturer " + manufacturer
+                + " \n model " + model
+                + " \n version " + version
+                + " \n versionRelease " + versionRelease
+                + "\n Device Secure "+ deviceSecure
+        );
     }
 
     public void checkPerm(View view){

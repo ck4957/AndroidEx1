2a1ca049568d685a3371bea2c668cbe0112b42bf
Chirag Kular <ck4957@rit.edu>
2018-06-11 19:45:38 -0400

    Add PlayStore MetaData

--- app/src/main/java/com/example/chiragkular/androidex1/MainActivity.java
+++ app/src/main/java/com/example/chiragkular/androidex1/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.chiragkular.androidex1;
 
 import android.Manifest;
+import android.app.DownloadManager;
 import android.app.KeyguardManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.content.pm.PermissionInfo;
 import android.os.Build;
 import android.provider.Settings;
 import android.support.v4.content.ContextCompat;
@@ -17,14 +20,22 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity {
 
     public static final int REQUEST_CALENDAR = 0;
     public static final String EXTRA_MESSAGE = "com.example.chiragkular.MESSAGE";
-
+    private static final String TAG = "MyActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,33 +53,103 @@ public class MainActivity extends AppCompatActivity {
 //        startActivity(intent);
 //    }
 
-    public void analyzeSystem(View view) throws Settings.SettingNotFoundException {
+    public void analyzeSystem(View view) throws IOException {
 
-        DeviceSecure ds = new DeviceSecure();
-        int deviceSecure_Score = ds.DeviceSecureAnalysis();
-        TextView score_deviceSecure = findViewById(R.id.txtScore_deviceSecure);
-        score_deviceSecure.setText(String.valueOf(deviceSecure_Score));
-
-        DeviceVersion dv= new DeviceVersion();
-        int deviceVersion_Score = dv.DeviceVersionAnalysis();
-        TextView score_deviceVersion = findViewById(R.id.txtScore_deviceVersion);
-        score_deviceVersion.setText(String.valueOf(deviceVersion_Score));
-
+//        DeviceSecure ds = new DeviceSecure();
+//        int deviceSecure_Score = ds.DeviceSecureAnalysis();
+//        TextView score_deviceSecure = findViewById(R.id.txtScore_deviceSecure);
+//        score_deviceSecure.setText(String.valueOf(deviceSecure_Score));
+//
+//        DeviceVersion dv= new DeviceVersion();
+//        int deviceVersion_Score = dv.DeviceVersionAnalysis();
+//        TextView score_deviceVersion = findViewById(R.id.txtScore_deviceVersion);
+//        score_deviceVersion.setText(String.valueOf(deviceVersion_Score));
+        getAllApps(getApplicationContext());
+        //parseHTML("com.google.android.youtube");
+        //parseHTML("com.igorkh.trustcheck.securitycheck");
+        //new PlayStoreScraping().execute();
     }
 
-
+    public void parseHTML2(String pkgName){
+//        OkHttpClient client = new OkHttpClient();
+//
+//        DownloadManager.Request request = new DownloadManager.Request.Builder()
+//                .url("https://api.apptweak.com/android/applications/com.google.android.youtube.json?country=us&language=en")
+//                .get()
+//                .addHeader("x-apptweak-key", "wXmJE3huo2WZtpA8-B1Sn2koG2c")
+//                .addHeader("cache-control", "no-cache")
+//                .addHeader("postman-token", "c65b9b60-ff7d-5e60-ba71-ffc05442a26f")
+//                .build();
+//
+//        Response response = client.newCall(request).execute();
+    }
+//    public void parseHTML(String pkgName) throws IOException {
+//
+//        Document doc = Jsoup.connect("https://play.google.com/store/apps/details?id="+pkgName)
+//                //.timeout(60000)
+////                .userAgent("Mozilla/5.0 (Windows; U; WindowsNT 5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6")
+////                .referrer("http://www.google.com")
+//                .get();
+//        Log.i("Doc ",doc.title());
+////        Elements newsHeadlines = doc.select("div.pf5lIe");
+////        for (Element headline : newsHeadlines) {
+////            //Log.i("%s\n\t%s",newsHeadlines.get(0).attr("aria-label"));
+////            Log.e("headline",headline.data());
+////        }
+//    }
 
 
     public void getAllApps(Context context){
-        PackageManager packageManager = context.getPackageManager();
-        List<PackageInfo> applist = packageManager.getInstalledPackages(PackageManager.GET_PERMISSIONS);
-        for (PackageInfo app:applist) {
-            if(app.packageName == "com.google.android.youtube")
-            Log.e("Apps","App Name:"+app.packageName
-                +"\n App Info "+ app.applicationInfo
-                    +"\n App Permission "+ app.permissions
-                    +"\n App RequestedPermissions "+ printLoops(app.requestedPermissions)
-                    +"\n App RequestedPermissionsFlags "+ printLoops(app.requestedPermissionsFlags));
+
+        final PackageManager pm = getPackageManager();
+        final List<ApplicationInfo> installedApps = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+
+        for ( ApplicationInfo app : installedApps ) {
+            //Details:
+            //Log.d(TAG, "Package: " + app.packageName);
+            Log.e("", app.packageName);
+            //Log.d(TAG, "UID: " + app.uid);
+            //Log.d(TAG, "Directory: " + app.sourceDir);
+
+//            //Permissions:
+//            StringBuffer permissions = new StringBuffer();
+//
+//            try {
+//                PackageInfo packageInfo = pm.getPackageInfo(app.packageName, PackageManager.GET_PERMISSIONS);
+//                String[] requestedPermissions = packageInfo.requestedPermissions;
+//                if (requestedPermissions != null) {
+//                    for (int i = 0; i < requestedPermissions.length; i++) {
+//                        permissions.append(requestedPermissions[i] + "\n");
+//                        String a = requestedPermissions[i];
+//                        //Log.i("-Requested Permissions",a);
+//                        if (a.contains("android.permission.")) {
+//                            PermissionInfo pi = getPackageManager().getPermissionInfo(a, PackageManager.GET_META_DATA);
+//                            Log.i("--Permission Info",String.valueOf(pi.protectionLevel));
+//                            String protctionLevel;
+//                            switch (pi.protectionLevel) {
+//                                case PermissionInfo.PROTECTION_NORMAL:
+//                                    protctionLevel = "normal";
+//                                    break;
+//                                case PermissionInfo.PROTECTION_DANGEROUS:
+//                                    protctionLevel = "dangerous";
+//                                    break;
+//                                case PermissionInfo.PROTECTION_SIGNATURE:
+//                                    protctionLevel = "signature";
+//                                    break;
+//                                default:
+//                                    protctionLevel = "<unknown>";
+//                                    break;
+//                            }
+//                            Log.e("--Protection Level", pi.name + ": " + protctionLevel);
+//                            //list_permission.add(a+"        "+protctionLevel);
+//                        }
+//                        //Log.d(TAG, "Permissions: " + permissions);
+//                    }
+//                }
+//            }
+//            catch ( PackageManager.NameNotFoundException e) {
+//                e.printStackTrace();
+//            }
         }
     }
 
